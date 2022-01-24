e5c2ded2608e49818a3ff5b8acba9b2840c5eba1
Neo Lichaba <neolicham@gmail.com>
2022-01-24 12:50:52 +0200

    test commit

---
+++ Chap8/Chap8.js
@@ -0,0 +1,11 @@
+var theCar= {
+    Brand:"Maserati",
+    Model:"Ghilbi",
+    Year:2021,
+    Condition: "Immaculate"
+}
+
+document.getElementById("brand").innerHTML=theCar.Brand;
+document.getElementById("model").innerHTML=theCar.Model;
+document.getElementById("year").innerHTML=theCar.Year;
+document.getElementById("condition").innerHTML=theCar.Condition;
